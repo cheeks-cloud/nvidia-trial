cacd5c9d70d1a79794a8c524d0e85cb56d0be203
@cheeks cloud <agneskaswii700@gmail.com>
2022-02-25 06:20:43 +0300

    ft:creating cards inside rows with content

---
+++ index.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    var element = document.getElementById("myCarousel");
+    var myCarousel = new bootstrap.Carousel(element, {
+        interval: 1000
+    });
+});
